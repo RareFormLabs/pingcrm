393a8228c13a2e583a92ca61d8f732db5dd80a14
Chase Giunta <me@chasegiunta.com>
2024-12-21 10:47:37 -0600

    Update message on Dashboard

--- src/js/Pages/Dashboard/Index.vue
+++ src/js/Pages/Dashboard/Index.vue
@@ -7,9 +7,21 @@
       <a
         class="text-indigo-500 hover:text-orange-600 underline"
         href="https://inertiajs.com"
+        target="_blank"
         >Inertia.js</a
       >
+      on the
+      <a
+        href="https://github.com/RareFormLabs/craft-inertia"
+        target="_blank"
+        class="text-indigo-500 hover:text-orange-600 underline"
+        >Craft CMS adapter</a
+      >
       works.
+    </p>
+    <p class="mb-8 leading-normal">
+      The database is reset every hour, so feel free to play around as much as
+      you like, but please be mindful of others.
     </p>
   </div>
 </template>
