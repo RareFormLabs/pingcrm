cee28f3f120be3c1fd24d99f905c400a115d0f2c
Chase Giunta <me@chasegiunta.com>
2025-04-19 15:55:02 -0500

    vite fixes

--- config/vite.php
+++ config/vite.php
@@ -2,15 +2,17 @@
 
 use craft\helpers\App;
 
+$isDevEnv = App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev';
+
 return [
-  'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
+  'useDevServer' => $isDevEnv,
   'manifestPath' => '@webroot/dist/.vite/manifest.json',
   'devServerPublic' => 'http://localhost:5173/',
   'serverPublic' => App::env('PRIMARY_SITE_URL') . '/dist/',
   'errorEntry' => '',
   'cacheKeySuffix' => '',
-  'devServerInternal' => '',
-  'checkDevServer' => false,
+  'devServerInternal' => 'http://localhost:5173/',
+  'checkDevServer' => $isDevEnv,
   'includeReactRefreshShim' => false,
   'includeModulePreloadShim' => true,
   'criticalPath' => '@webroot/dist/criticalcss',
